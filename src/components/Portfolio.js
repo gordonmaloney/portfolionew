ccac460731dece27d99cccbeea3728cfa0688646
gordonmaloney <gordonmaloney@gmail.com>
2022-05-12 14:45:40 +0100

    render projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,19 +3,23 @@ import { Link } from "react-router-dom";
 import { AnimatedPage } from "./AnimatedPage";
 import { Blob } from "./Blob";
 import { SquareBlob } from "./SquareBlob";
+import { Button, Grid, Hidden } from "@mui/material";
+import { ProjectBoxLeft } from "./ProjectBox/ProjectBoxLeft";
+import { ProjectBoxRight } from "./ProjectBox/ProjectBoxRight";
 
 export const Portfolio = () => {
   const [xExit, setxExit] = useState();
   const [yExit, setyExit] = useState(0);
 
+  const [more, setMore] = useState(false);
 
   //set shape of square blob
-  const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
 
   function getWindowDimensions() {
     const { innerWidth: width } = window;
     return {
-      width
+      width,
     };
   }
 
@@ -26,12 +30,9 @@ export const Portfolio = () => {
       setScreenWidth(getWindowDimensions());
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
-
-  console.log(screenWidth.width > 800 ? 1 : screenWidth.width > 300 ? 2-(((screenWidth.width/100)/4)-1) : 2)
-
 
   return (
     <AnimatedPage
@@ -41,81 +42,138 @@ export const Portfolio = () => {
         exit: { opacity: 0, y: 0, x: xExit },
       }}
     >
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        height: "100vh",
-        width: "100vw",
-        scroll: "auto",
-        overflow: "scroll",
-paddingTop: "100px"
-      }}
-    >
-      
-      <div style={{ width: "70%", maxWidth: "600px", minWidth: "300px" }}>
-        <div style={{ float: "left" }}>
-          <div
-            style={{
-              position: "relative",
-              width: "350px",
-              maxWidth: "80%",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-              pointerEvents: "none" 
-            }}
-          >
-            <Blob colour="#8bd3dd" radius={90} squash={2.2} />
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          height: "100vh",
+          width: "100vw",
+          scroll: "auto",
+          overflow: "scroll",
+          paddingTop: "100px",
+        }}
+      >
+        <div style={{ width: "70%", maxWidth: "800px", minWidth: "300px" }}>
+          <div style={{ float: "left" }}>
+            <div
+              style={{
+                position: "relative",
+                width: "350px",
+                maxWidth: "80%",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                pointerEvents: "none",
+              }}
+            >
+              <Blob colour="#8bd3dd" radius={90} squash={2.2} />
 
-            <Blob colour="#f3d2c1" radius={85} squash={2.3} />
+              <Blob colour="#f3d2c1" radius={85} squash={2.3} />
 
-            <Blob colour="#001858" radius={78} squash={2.5} />
+              <Blob colour="#001858" radius={78} squash={2.5} />
 
-            <h1 className="subHeader">My work</h1>
+              <h1 className="subHeader">My work</h1>
+            </div>
           </div>
-        </div>
-        <br /> <br />
-        <br />
-        <br />
-        <center>
-          <p style={{ width: "90%" }}>
-            Here's some of my work...
-          </p>
+          <br /> <br />
+          <br />
+          <br />        <br /> <br />
+          <br />
+          <center>
+            Check out some of my projects below, and feel free to play with the fully-functional, live versions of them all:
+          </center>
+          <br /><br />
+          <ProjectBoxLeft
+            desc="whatLuck is the one-stop potluck organising app"
+            url="https://whatluck.netlify.app"
+          />
+          <ProjectBoxRight
+            desc="Lingr is a microblogging site for language learners"
+            url="https://lingr.netlify.app"
+          />
+          <ProjectBoxLeft
+            desc="Gobbleblog is the answer to never getting a bad takeaway twice"
+            url="https://gobbleblog.netlify.app"
+          />
+          <ProjectBoxRight
+            desc="Flashback is a spaced-repetition flashcard app for memorising anything"
+            url="https://flashbackflashcards.netlify.app/"
+          />
+          {!more && (
+            <center>
+              <div
+                style={{
+                  position: "relative",
+                  width: "350px",
+                  maxWidth: "80%",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Blob colour="#f3d2c1" radius={85} squash={2.3} />
 
-          
-        </center>
+                <h1
+                  onClick={() => setMore(true)}
+                  style={{ zIndex: 5, cursor: "pointer" }}
+                >
+                  Show more{" "}
+                </h1>
+              </div>
+            </center>
+          )}
+          {more && (
+            <>
+              <ProjectBoxLeft
+                desc="Make Space is a tool to stop men dominating meetings"
+                url="https://make-space.netlify.app"
+              />
 
-<div style={{width: "100%", height: "500px", 
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",            position: "relative",
-}}>
+              <ProjectBoxRight
+                desc="Facle is a Scottish Gaelic Wordle clone"
+                url="https://facle.netlify.app/"
+              />
 
-        <SquareBlob colour="#8bd3dd" radius={90} squash={1} squareScale={screenWidth.width > 800 ? 1 : screenWidth.width > 400 ? 2 - (((screenWidth.width/100)/4)-1) : 2} />
+              <ProjectBoxLeft
+                desc="Cuimhne is a Gaelic-language memory game"
+                url="https://cuimhne.netlify.app"
+              />
 
-        <div style={{width: "100%"}} className="iframeWrap">
+              <ProjectBoxRight
+                desc="GymBud is an app for gym-users to record and track their progress"
+                url="https://gym-bud.netlify.app"
+              />
+            
 
+<center>
+<div
+  style={{
+    position: "relative",
+    width: "350px",
+    maxWidth: "80%",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  }}
+>
+  <Blob colour="#f3d2c1" radius={85} squash={2.3} />
 
-<iframe className="scaled-frame" src="http://whatluck.netlify.app" ></iframe>
-
+  <h1
+    onClick={() => setMore(false)}
+    style={{ zIndex: 5, cursor: "pointer" }}
+  >
+    Show Less{" "}
+  </h1>
 </div>
-</div>
-
+</center>
+</>
+          )}
           <div style={{ float: "left" }} onClick={() => setxExit(100)}>
             <Link to="../l/contact">Get in touch</Link>
           </div>
-
           <div style={{ float: "right" }} onClick={() => setxExit(-100)}>
             <Link to="../r/about">About Me</Link>
           </div>
-          <br />
-
-          <center>
-            <div onClick={() => setyExit(-100)}>
-              <Link to="../u/home">Home</Link>
-            </div>
-          </center>
         </div>
       </div>
     </AnimatedPage>
---
+++ src/components/ProjectBox/ProjectBoxLeft.js
@@ -0,0 +1,95 @@
+import React, {useEffect, useState} from 'react'
+import { Grid } from '@mui/material'
+import { Blob } from '../Blob'
+import { SquareBlob } from '../SquareBlob'
+
+export const ProjectBoxLeft = ({desc, url}) => {
+
+    
+//set shape of square blob
+const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+
+function getWindowDimensions() {
+  const { innerWidth: width } = window;
+  return {
+    width,
+  };
+}
+
+useEffect(() => {
+  setScreenWidth(getWindowDimensions());
+
+  function handleResize() {
+    setScreenWidth(getWindowDimensions());
+  }
+
+  window.addEventListener("resize", handleResize);
+  return () => window.removeEventListener("resize", handleResize);
+}, []);
+
+
+  return (
+    
+    <Grid container alignItems={"center"}>
+    <Grid item xs={6} md={12}></Grid>
+
+    <Grid
+      item
+      xs={6}
+      md={4}
+      style={{ zIndex: 5, display: "flex", justifyContent: "flex-end" }}
+    >
+      <div
+        style={{
+          position: "relative",
+          width: "100%",
+          maxWidth: "200px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-start",
+          pointerEvents: "none",
+        }}
+      >
+        <Blob colour="#001858" radius={95} squash={1} />
+        <p className="projectDesc">
+{desc}          <br />
+          <br />
+          read more
+        </p>
+      </div>
+    </Grid>
+    <Grid item xs={12} md={8}>
+      <div
+        style={{
+          width: "100%",
+          height: "500px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          position: "relative",
+        }}
+      >
+        <SquareBlob
+          colour="#8bd3dd"
+          radius={90}
+          squash={1}
+          squareScale={
+            screenWidth.width > 800
+              ? 1
+              : screenWidth.width > 400
+              ? 2 - (screenWidth.width / 100 / 4 - 1)
+              : 2
+          }
+        />
+
+        <div style={{ width: "100%" }} className="iframeWrap">
+          <iframe
+            className="scaled-frame"
+            src={url}
+          ></iframe>
+        </div>
+      </div>
+    </Grid>
+  </Grid>
+  )
+}
